77ecafc30972050add1c455b3f624689db80b17e
Delivergo <Delivergo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-25 11:03:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/web2print-beta/trunk@536137 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 0.8/Web_2_Print.php
@@ -0,0 +1,9 @@
+<?php 	
+	/*
+        Plugin Name: Web to Print Plugin v0.8
+        Plugin URI: http://www.delivergo.com
+        Description: Plugin, which allows you to sell products on your wordpress blog, using the delivergo web to print API.
+        Author: Normprint Ltd.    		
+		Version: 0.8
+    */	
+?>
